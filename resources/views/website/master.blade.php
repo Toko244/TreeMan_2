b67589f9e68325dc2de9c73300c4cc2d45d69d14
davit gordadze <davitgordadze1994@gmail.com>
2022-12-22 15:55:14 +0400

    davita header

--- resources/views/website/master.blade.php
+++ resources/views/website/master.blade.php
@@ -1,7 +1,12 @@
 <html lang="{{app()->getLocale()}}">
 <head>
-
+    @include('components.head')
 </head>
+
+<header>
+    @include('components.header')
+</header>
+
 <body>
     @yield('master')
 </body>
