17767202734b256c3dc310b996c1362dc1f8e8e8
davit gordadze <davitgordadze1994@gmail.com>
2022-12-22 16:10:28 +0400

    home slider

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -11,7 +11,7 @@
     
                     <div class="social flex items-center">
                         <div class="icon flex items-center h-full" style="margin: 0 11px;">
-                            <a href="{{settings('facebook')}}" class="flex items-center justify-center rounded transition-duration shadow" style="width: 40px; height: 40px;">
+                            <a href="{{settings('facebook')}}" target="_blank" class="flex items-center justify-center rounded transition-duration shadow" style="width: 40px; height: 40px;">
                                 <svg width="9" height="18" viewBox="0 0 9 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M2.52306 17.8521H5.95551V9.03407H8.35008L8.60446 6.08183H5.95551V4.39709C5.95551 3.70084 6.09203 3.42885 6.74833 3.42885H8.60616V0.365723H6.23194C3.6771 0.365723 2.5256 1.51878 2.5256 3.72812V6.08183H0.737305V9.07455H2.52306V17.8521Z" fill="#006838"/>
                                 </svg>
@@ -19,7 +19,7 @@
                         </div>
     
                         <div class="icon flex items-center h-full" style="margin: 0 11px;">
-                            <a href="{{settings('linkedin')}}" class="flex items-center justify-center rounded transition-duration shadow" style="width: 40px; height: 40px;">
+                            <a href="{{settings('linkedin')}}" target="_blank" class="flex items-center justify-center rounded transition-duration shadow" style="width: 40px; height: 40px;">
                                 <svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M3.95509 5.49658H0.541568C0.468901 5.49658 0.399211 5.52568 0.347828 5.57748C0.296445 5.62927 0.267578 5.69953 0.267578 5.77278V16.8262C0.267578 16.8994 0.296445 16.9697 0.347828 17.0215C0.399211 17.0733 0.468901 17.1024 0.541568 17.1024H3.95451C4.02717 17.1024 4.09686 17.0733 4.14825 17.0215C4.19963 16.9697 4.2285 16.8994 4.2285 16.8262V5.77278C4.2285 5.69963 4.19971 5.62946 4.14845 5.57768C4.0972 5.5259 4.02765 5.49674 3.95509 5.49658Z" fill="#006838"/>
                                     <path d="M2.2495 7.58068e-08C1.8045 0.000116046 1.36953 0.133239 0.999583 0.382537C0.629635 0.631834 0.341326 0.986111 0.171112 1.40057C0.000897201 1.81503 -0.043579 2.27106 0.0433067 2.711C0.130192 3.15094 0.344538 3.55503 0.659241 3.87218C0.973945 4.18932 1.37487 4.40528 1.81133 4.49275C2.24779 4.58022 2.70018 4.53527 3.11129 4.36358C3.52241 4.1919 3.87379 3.90118 4.121 3.5282C4.36822 3.15522 4.50017 2.71672 4.50017 2.26815C4.50017 1.97024 4.44195 1.67525 4.32884 1.40003C4.21572 1.12481 4.04993 0.874742 3.84093 0.664117C3.63193 0.453493 3.38381 0.286435 3.11075 0.172484C2.83769 0.0585336 2.54504 -7.69424e-05 2.2495 7.58068e-08Z" fill="#006838"/>
@@ -29,7 +29,7 @@
                         </div>
     
                         <div class="icon flex items-center h-full" style="margin: 0 11px;">
-                            <a href="{{settings('youtube')}}" class="flex items-center justify-center rounded transition-duration shadow" style="width: 40px; height: 40px;">
+                            <a href="{{settings('youtube')}}" target="_blank" class="flex items-center justify-center rounded transition-duration shadow" style="width: 40px; height: 40px;">
                                 <svg width="21" height="15" viewBox="0 0 21 15" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M8.12153 10.6291V4.81051L13.6236 7.71032L8.12153 10.6291ZM15.0798 0.623231H5.33393C0 0.623231 0 2.16537 0 5.9584V9.66483C0 13.272 0.75559 15 5.33393 15H15.0798C19.2152 15 20.4137 13.9902 20.4137 9.66483V5.9584C20.4137 1.96595 20.2124 0.623231 15.0798 0.623231Z" fill="#006838"/>
                                 </svg>
