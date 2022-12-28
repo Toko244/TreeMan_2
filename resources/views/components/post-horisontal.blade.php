cf86a74486522617e7ebaecb161c17a6a598ff0b
davit gordadze <davitgordadze1994@gmail.com>
2022-12-28 16:20:40 +0400

    IT-4

--- resources/views/components/post-horisontal.blade.php
+++ resources/views/components/post-horisontal.blade.php
@@ -3,6 +3,9 @@
         <div class="row_posts component-padding-100-120">
             <div class="container">
                 <div class="flex items-center justify-start post_row">
+
+
+                    
                     <div class="row_post flex items-center justify-center hidden hover">
                         <a href="" class="w-full h-full flex items-center justify-center relative">
                             <img src="assets/img/Mask group.png" alt="" class="w-full h-full cover relative hover-image">
@@ -19,21 +22,7 @@
                         </a>
                     </div>
     
-                    <div class="row_post flex items-center justify-center hidden hover">
-                        <a href="" class="w-full h-full flex items-center justify-center relative">
-                            <img src="assets/img/Mask group.png" alt="" class="w-full h-full cover relative hover-image">
-                            <img src="assets/img/post-cover.png" alt="" class="post-cover w-full">
-
-                            <div class="post_arrow flex items-center justify-start absolute">
-                                <div class="flex items-center semibold white mr-1">
-                                    SEEDLNGS
-                                </div>
-                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M12.172 8.77766L-5.92552e-07 8.77767L-7.67397e-07 6.77767L12.172 6.77766L6.808 1.41366L8.222 -0.000335013L16 7.77766L8.222 15.5557L6.808 14.1417L12.172 8.77766Z" fill="white"/>
-                                </svg>
-                            </div>
-                        </a>
-                    </div>
+               
                 </div>
             </div>
         </div>
