05409edb53d4f1ac714b0e3ea3ca448f1e404b7c
davit gordadze <davitgordadze1994@gmail.com>
2023-01-16 14:49:06 +0400

    davita card banner

--- resources/views/components/front-card-banner.blade.php
+++ resources/views/components/front-card-banner.blade.php
@@ -3,14 +3,47 @@
         <div class="container">
             <div class="row">
 
-                <div class="col-lg-4">
+                <div class="col-lg-4 col-md-6 col-sm-12">
                     <div class="card flex items-center justify-center relative w-full hidden mb-2 shadow transition-duration">
-                        <a href="" class="w-full h-full relative">
+                        <a href="" class="w-full h-full relative flex items-center justify-center">
+                            <img src="assets/img/card.png" alt="" class="w-full h-full cover relative">
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-sm-12">
+                    <div class="card flex items-center justify-center relative w-full hidden mb-2 shadow transition-duration">
+                        <a href="" class="w-full h-full relative flex items-center justify-center">
+                            <img src="assets/img/card.png" alt="" class="w-full h-full cover relative">
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-sm-12">
+                    <div class="card flex items-center justify-center relative w-full hidden mb-2 shadow transition-duration">
+                        <a href="" class="w-full h-full relative flex items-center justify-center">
+                            <img src="assets/img/card.png" alt="" class="w-full h-full cover relative">
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-sm-12">
+                    <div class="card flex items-center justify-center relative w-full hidden mb-2 shadow transition-duration">
+                        <a href="" class="w-full h-full relative flex items-center justify-center">
+                            <img src="assets/img/card.png" alt="" class="w-full h-full cover relative">
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-sm-12">
+                    <div class="card flex items-center justify-center relative w-full hidden mb-2 shadow transition-duration">
+                        <a href="" class="w-full h-full relative flex items-center justify-center">
                             <img src="assets/img/card.png" alt="" class="w-full h-full cover relative">
                         </a>
                     </div>
                 </div>
                 
+
             </div>
         </div>
     </div>
