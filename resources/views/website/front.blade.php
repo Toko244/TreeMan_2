f976acd33485f2081a81776b38126b0761cc24ad
davit gordadze <davitgordadze1994@gmail.com>
2023-02-13 16:57:36 +0400

    front card banner

--- resources/views/website/front.blade.php
+++ resources/views/website/front.blade.php
@@ -415,7 +415,24 @@
                     <div class="col-lg-4 col-md-6 col-sm-12">
                         <div class="card flex items-center justify-center relative w-full hidden mb-2 shadow transition-duration">
                             <a href="" class="w-full h-full relative flex items-center justify-center">
-                                <img src="/assets/img/card.png" alt="" class="w-full h-full cover relative">
+                                <img src="assets/img/card-brown.png" alt="" class="w-full h-full cover absolute top-0 left-0 brown">
+                                <img src="assets/img/card-white.png" alt="" class="w-full h-full cover absolute top-0 left-0 white">
+                                <div class="relative z-10 text-xl card-title pl-2 pr-3 bold">
+                                    When cut down, it grows again (can be cut down 8-10 times)
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+
+                 
+                    <div class="col-lg-4 col-md-6 col-sm-12">
+                        <div class="card flex items-center justify-center relative w-full hidden mb-2 shadow transition-duration">
+                            <a href="" class="w-full h-full relative flex items-center justify-center">
+                                <img src="assets/img/card-brown.png" alt="" class="w-full h-full cover absolute top-0 left-0 brown">
+                                <img src="assets/img/card-white.png" alt="" class="w-full h-full cover absolute top-0 left-0 white">
+                                <div class="relative z-10 text-xl card-title pl-2 pr-3 bold">
+                                    When cut down, it grows again (can be cut down 8-10 times)
+                                </div>
                             </a>
                         </div>
                     </div>
@@ -423,7 +440,11 @@
                     <div class="col-lg-4 col-md-6 col-sm-12">
                         <div class="card flex items-center justify-center relative w-full hidden mb-2 shadow transition-duration">
                             <a href="" class="w-full h-full relative flex items-center justify-center">
-                                <img src="/assets/img/card.png" alt="" class="w-full h-full cover relative">
+                                <img src="assets/img/card-brown.png" alt="" class="w-full h-full cover absolute top-0 left-0 brown">
+                                <img src="assets/img/card-white.png" alt="" class="w-full h-full cover absolute top-0 left-0 white">
+                                <div class="relative z-10 text-xl card-title pl-2 pr-3 bold">
+                                    When cut down, it grows again (can be cut down 8-10 times)
+                                </div>
                             </a>
                         </div>
                     </div>
@@ -431,7 +452,11 @@
                     <div class="col-lg-4 col-md-6 col-sm-12">
                         <div class="card flex items-center justify-center relative w-full hidden mb-2 shadow transition-duration">
                             <a href="" class="w-full h-full relative flex items-center justify-center">
-                                <img src="/assets/img/card.png" alt="" class="w-full h-full cover relative">
+                                <img src="assets/img/card-brown.png" alt="" class="w-full h-full cover absolute top-0 left-0 brown">
+                                <img src="assets/img/card-white.png" alt="" class="w-full h-full cover absolute top-0 left-0 white">
+                                <div class="relative z-10 text-xl card-title pl-2 pr-3 bold">
+                                    When cut down, it grows again (can be cut down 8-10 times)
+                                </div>
                             </a>
                         </div>
                     </div>
@@ -439,7 +464,11 @@
                     <div class="col-lg-4 col-md-6 col-sm-12">
                         <div class="card flex items-center justify-center relative w-full hidden mb-2 shadow transition-duration">
                             <a href="" class="w-full h-full relative flex items-center justify-center">
-                                <img src="/assets/img/card.png" alt="" class="w-full h-full cover relative">
+                                <img src="assets/img/card-brown.png" alt="" class="w-full h-full cover absolute top-0 left-0 brown">
+                                <img src="assets/img/card-white.png" alt="" class="w-full h-full cover absolute top-0 left-0 white">
+                                <div class="relative z-10 text-xl card-title pl-2 pr-3 bold">
+                                    When cut down, it grows again (can be cut down 8-10 times)
+                                </div>
                             </a>
                         </div>
                     </div>
@@ -447,12 +476,14 @@
                     <div class="col-lg-4 col-md-6 col-sm-12">
                         <div class="card flex items-center justify-center relative w-full hidden mb-2 shadow transition-duration">
                             <a href="" class="w-full h-full relative flex items-center justify-center">
-                                <img src="/assets/img/card.png" alt="" class="w-full h-full cover relative">
+                                <img src="assets/img/card-brown.png" alt="" class="w-full h-full cover absolute top-0 left-0 brown">
+                                <img src="assets/img/card-white.png" alt="" class="w-full h-full cover absolute top-0 left-0 white">
+                                <div class="relative z-10 text-xl card-title pl-2 pr-3 bold">
+                                    When cut down, it grows again (can be cut down 8-10 times)
+                                </div>
                             </a>
                         </div>
                     </div>
-                    
-
                 </div>
             </div>
         </div>
