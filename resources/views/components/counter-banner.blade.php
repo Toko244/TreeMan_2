a5af8dbcbc76a714f623995be9c996ea44bdd98a
davit gordadze <davitgordadze1994@gmail.com>
2023-02-20 19:00:26 +0400

    davita count component

--- resources/views/components/counter-banner.blade.php
+++ resources/views/components/counter-banner.blade.php
@@ -1,22 +1,27 @@
 @if(isset($components) && count($components) > 0)
 <section>
-    <div class="count-section section-padding">
+    <div class="count-section section-padding" id="counter">
         <div class="container">
             <div class="row">
                 @foreach ($components as $key => $component)
-                @if($key < 4)
-                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-                    <div class="count flex column w-full">
-                        <div class="dot"></div>
-                        <div class="title lemon-medium text-2xl green mb-1">
-                            {{$component->translate(app()->getlocale())->title}}
-                        </div>
-                        <div class="number green text-4xl lemon-medium">
-                            {{$component->translate(app()->getlocale())->value}}
+                    @if($key < 4)
+                    <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                        <div class="count flex column w-full">
+                            <div class="dot"></div>
+                            <div class="title lemon-medium text-2xl green mb-1">
+                                {{$component->translate(app()->getlocale())->title}}
+                            </div>
+                            <div class="flex items-center justify-start">
+                                <div class="number green text-4xl lemon-medium counter-value" data-count="{{$component->translate(app()->getlocale())->value}}K">
+                                    0
+                                </div>
+                                <div class="number green text-4xl lemon-medium plus">
+                                    +
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                @endif
+                    @endif
                 @endforeach
             </div>
         </div>
