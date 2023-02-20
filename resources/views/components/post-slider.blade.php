44003fc7ce59d18d5ec2c5655af63ec4d4920f44
davit gordadze <davitgordadze1994@gmail.com>
2023-02-20 15:30:38 +0400

    davita wvrilamnebi

--- resources/views/components/post-slider.blade.php
+++ resources/views/components/post-slider.blade.php
@@ -8,13 +8,13 @@
 
             <div class="info flex items-center justify-end">
                 <div class="arrow flex items-center">
-                    <div class="prev flex items-center" id="prev">
+                    <div class="prev flex items-center">
                         <svg width="34" height="20" viewBox="0 0 34 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M4.87034 8.71432L34 8.71432L34 11.2857L4.87034 11.2857L11.6949 18.1821L9.89589 20L2.22735e-08 10L9.89589 2.26179e-08L11.6949 1.81795L4.87034 8.71432Z" fill="#006838"/>
                         </svg>
                     </div>
 
-                    <div class="next flex items-center" id="next">
+                    <div class="next flex items-center">
                         <svg width="34" height="20" viewBox="0 0 34 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M29.1297 11.2857L4.55712e-06 11.2857L4.77619e-06 8.71432L29.1297 8.71432L22.3051 1.81795L24.1041 1.0196e-06L34 10L24.1041 20L22.3051 18.1821L29.1297 11.2857Z" fill="#006838"/>
                         </svg>
@@ -32,19 +32,19 @@
     </div>
     
     <div class="w-full hidden">
-       <div class="container">
+        <div class="container">
             <div class="post-slider" style="width: 130%;">
                 @if(count($component->files) > 0)
-                @foreach($component->files as $key => $image)
-                <div class="slide">
-                    <a href="@if($image->title != '') {{$image->title}} @else /uploads/img/{{$image->file}} @endif" class="block w-full h-full"  data-fancybox="photo">
-                        <img src="/uploads/img/{{$image->file}}" alt="" class="w-full h-full cover">
-                    </a>
-                </div>
-                @endforeach
-            @endif
-
-       </div>
+                    @foreach($component->files as $key => $image)
+                    <div class="slide">
+                        <a href="@if($image->title != '') {{$image->title}} @else /uploads/img/{{$image->file}} @endif" class="block w-full h-full"  data-fancybox="photo">
+                            <img src="/uploads/img/{{$image->file}}" alt="" class="w-full h-full cover">
+                        </a>
+                    </div>
+                    @endforeach
+                @endif
+            </div>
+        </div>
     </div>
 </div>
 
