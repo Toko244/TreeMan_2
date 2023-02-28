36501bed19b3a5ee8881e58b8aace727148fe42a
kasaba <kasaburi.t@gmail.com>
2023-02-28 20:34:07 +0400

    fixing scroll_content

--- resources/views/website/text-page.blade.php
+++ resources/views/website/text-page.blade.php
@@ -40,7 +40,7 @@
                                             <path d="M4.87034 8.71432L34 8.71432L34 11.2857L4.87034 11.2857L11.6949 18.1821L9.89589 20L2.22735e-08 10L9.89589 2.26179e-08L11.6949 1.81795L4.87034 8.71432Z" fill="white"/>
                                         </svg>
                                     </div>
-    
+
                                     <div class="next text-next background-green">
                                         <svg width="34" height="20" viewBox="0 0 34 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M29.1297 11.2857L4.55712e-06 11.2857L4.77619e-06 8.71432L29.1297 8.71432L22.3051 1.81795L24.1041 1.0196e-06L34 10L24.1041 20L22.3051 18.1821L29.1297 11.2857Z" fill="white"/>
@@ -63,7 +63,7 @@
                         </div>
                         <div class="list flex wrap w-full">
                             @foreach ($section->sectioncomponents() as $key => $item)
-                                @if($item->additional['scroll_content'] == 1)
+                                @if(isset($item->additional['scroll_content']) && $item->additional['scroll_content'] == 1)
                                 <div class="list-bullet relative flex items-center">
                                     <div class="circle"></div>
                                     <a href="#{{ $item->id }}" class="semibold white">
