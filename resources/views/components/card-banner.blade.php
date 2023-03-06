b3a19904ff13c66e621753ee725d0351f98051c9
irakli bandzeladze <iraklibandzeladze@gmail.com>
2023-03-06 18:01:08 +0400

    update 02

--- resources/views/components/card-banner.blade.php
+++ resources/views/components/card-banner.blade.php
@@ -8,7 +8,7 @@
             <div class="row row-resp-slider">
                 @foreach ($components as $key => $post)
                 <div class="col-lg-4 col-md-6 col-sm-12">
-                    <div class="card flex items-center justify-center relative w-full hidden mb-2 shadow transition-duration">
+                    <div class="card flex items-center justify-center relative w-full hidden mb-2 @if($post->translate(app()->getlocale())->redirect_link != '') shadow @endif transition-duration">
                         @if($post->translate(app()->getlocale())->redirect_link != '')
                         <a href="{{ $post->translate(app()->getlocale())->redirect_link }}" @if ($post->target_blank == 1) target="_blank" @endif class="w-full h-full relative flex items-center justify-center">                                
                             <img src="/assets/img/card-brown.png" alt="" class="w-full h-full cover absolute top-0 left-0 brown">
