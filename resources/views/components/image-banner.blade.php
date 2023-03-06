ffec15a8291794800b796b415a77988980906082
irakli bandzeladze <iraklibandzeladze@gmail.com>
2023-03-06 17:49:25 +0400

    car baner withot hove

--- resources/views/components/image-banner.blade.php
+++ resources/views/components/image-banner.blade.php
@@ -12,13 +12,13 @@
                         @if($post->translate(app()->getlocale())->redirect_link != '')
                         <a href="{{ $post->translate(app()->getlocale())->redirect_link }}" @if ($post->target_blank == 1) target="_blank" @endif class="w-full h-full relative flex items-center justify-center">
                         @else
-                        <a href="javascript:void(0)" class="w-full h-full relative flex items-center justify-center">
+                        <div class="w-full h-full relative flex items-center justify-center">
                         @endif    
                             <img src="/uploads/files/{{$post->image}}" alt="" class="w-full h-full cover absolute top-0 left-0">
                             <div class="relative z-10 text-xl card-title pl-2 pr-3 bold">
                                 {{$post->translate(app()->getlocale())->title}}
                             </div>
-                        </a>
+                        </div>
                     </div>
                 </div>
                 @endforeach
